3d90e27c28ba065e3f545043fa0270db6cf13b1c
kitsu <57687766+Kitsu42@users.noreply.github.com>
2026-09-21 17:13:34 +0000

    de novo a imagem

--- src/data/articles.ts
+++ src/data/articles.ts
@@ -20,7 +20,7 @@ export const articles: Article[] = [
     category: 'Math',
     tags: ['Math', 'Axiomas'],
     readTime: 9,
-    image: '../img/Articles/Axiomas.jpg',
+    image: '../../public/img/Articles/Axiomas.jpg',
     contentFile: 'axiomas.md',
   },
 ];
